38256aee9ccbddb11bd594ccb53c8c06656240c2
bwards <bwhinch@gmail.com>
2026-04-20 18:43:28 -0500

    Create questions-1.js

---
+++ questions-1.js
@@ -0,0 +1,15 @@
+const questions-1 = [
+  {
+    id: "Q1-001",
+    type: "mc",
+    question: "What happens if children drink a recommended dose of fluoride?",
+    options: [
+      "Bones become stronger",
+      "Growth will be stunted",
+      "Fewer dental cavities",
+      "Perform better in school",
+    ],
+    answer: 2,
+    tag: ["fluoride"]
+  }   
+];
