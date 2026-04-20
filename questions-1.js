5c94f4867c8f170889eb5affb5dd94dad9f82067
bwards <bwhinch@gmail.com>
2026-04-20 18:46:03 -0500

    Update questions-1.js

--- questions-1.js
+++ questions-1.js
@@ -11,5 +11,19 @@ const questions-1 = [
     ],
     answer: 2,
     tag: ["fluoride"]
-  }   
+  },
+  {
+    id: "Q1-002",
+    type: "tf",
+    question: "The feed rates determined by jar tests always produce the exact same results in the actual plant.",
+    answer: false,
+    tags: ["dose"]
+  },
+  {
+    id: "Q1-003",
+    type: "tf",
+    question: "All waters contain various amounts of total dissolved solids (TDS), including fresh water.",
+    answer: true,
+    tags: ["tds"]
+  }
 ];
