eea6a13e0df55d08c9d63a67afac6a1c8bfe32e9
bwards <bwhinch@gmail.com>
2026-04-20 20:50:45 -0500

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,365 @@
+const questions = [
+  {
+    "id": "2",
+    "type": "true_false",
+    "question": "Only one cell of iron bacteria is needed to start an infestation of iron bacteria in a well or distribution system.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "3",
+    "type": "true_false",
+    "question": "PH is decreased by the removal of iron dioxide.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "4",
+    "type": "true_false",
+    "question": "Fluoride saturators are a special application of a solution feeder.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "5",
+    "type": "true_false",
+    "question": "Fluoride solutions are extremely corrosive.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "6",
+    "type": "true_false",
+    "question": "Excessively hard water can cause scaling on the inside of pipes and thereby restrict flow.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "7",
+    "type": "true_false",
+    "question": "The stability of treated water is determined by measuring the pH and calculating the Langley index.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "8",
+    "type": "true_false",
+    "question": "The addition of acid to prevent calcium carbonate scale will accomplish the same things as re carbonation and the addition of polyphosphate.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "9",
+    "type": "true_false",
+    "question": "The feed rates determined by jar tests always produce the exact same results in the actual plant.",
+    "options": [],
+    "answer": "FALSE"
+  },
+  {
+    "id": "10",
+    "type": "true_false",
+    "question": "The effluent from an ion exchange unit usually will have 0 hardness until the unit needs regeneration.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "11",
+    "type": "true_false",
+    "question": "The overwhelming bulk of THM precursors in water are from industrial organic chemicals.",
+    "options": [],
+    "answer": "FALSE"
+  },
+  {
+    "id": "12",
+    "type": "true_false",
+    "question": "\"When collecting water samples for THM analysis",
+    "options": [
+      "FALSE"
+    ],
+    "answer": 0
+  },
+  {
+    "id": "13",
+    "type": "true_false",
+    "question": "\"The higher the pH of water",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "14",
+    "type": "true_false",
+    "question": "Low levels of arsenic consumed over a long period of time can cause cancer and non cancerous toxic effects on humans.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "15",
+    "type": "true_false",
+    "question": "Electrical conductivity (EC) can usually be correlated directly to arsenic reduction.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "16",
+    "type": "true_false",
+    "question": "\"All waters contain various amounts of total dissolved solids (TDS)",
+    "options": [
+      "TRUE"
+    ],
+    "answer": 0
+  },
+  {
+    "id": "17",
+    "type": "true_false",
+    "question": "The rate of hydrolysis is accelerated by decreased temperature.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "18",
+    "type": "true_false",
+    "question": "The output of a positive displacement pump may be throttled.",
+    "options": [],
+    "answer": "FALSE"
+  },
+  {
+    "id": "19",
+    "type": "true_false",
+    "question": "\"At a minimum",
+    "options": [],
+    "answer": "and it increases with both increasing and decreasing pH.\""
+  },
+  {
+    "id": "20",
+    "type": "true_false",
+    "question": "Trash and grit may be disposed of in landfills.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "21",
+    "type": "true_false",
+    "question": "\"Before filling a drained tank after removing sludge",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "22",
+    "type": "true_false",
+    "question": "The application of nitrogen fertilizers causes an increase in soil pH.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "23",
+    "type": "true_false",
+    "question": "One person may be permitted to attempt an emergency repair alone.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "24",
+    "type": "true_false",
+    "question": "Operators must be familiar with equipment and understand what it is intended to do before developing a preventative maintenance program and maintaining equipment.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "25",
+    "type": "true_false",
+    "question": "Check the temperature of a centrifugal pump bearing temperature by hand.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "26",
+    "type": "true_false",
+    "question": "Always be sure a centrifugal pump is primed before starting the pump.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "27",
+    "type": "true_false",
+    "question": "Diesel engines can be air or water cooled.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "28",
+    "type": "true_false",
+    "question": "Never pour water on a chlorine leak because this will only create a bigger problem by enlarging the leak.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "29",
+    "type": "true_false",
+    "question": "Analog instruments provide a direct numerical readout.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "30",
+    "type": "true_false",
+    "question": "Long detection lags can result in stable processes and good control.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "31",
+    "type": "true_false",
+    "question": "Entry into confined spaces is never permitted until the space has been properly ventilated using specially designed forced-air ventilators.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "32",
+    "type": "true_false",
+    "question": "\"Pneumatic instrumentation depends upon a constant source of clean",
+    "options": [],
+    "answer": "pressurized air for reliable operation.\""
+  },
+  {
+    "id": "33",
+    "type": "true_false",
+    "question": "The fumes from hydrofluorosilicic acid (H2SiF6) can corrode metal and etch glass on protective equipment.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "34",
+    "type": "true_false",
+    "question": "\"In reactions with most metals",
+    "options": [
+      "TRUE"
+    ],
+    "answer": 0
+  },
+  {
+    "id": "35",
+    "type": "true_false",
+    "question": "Anhydrous ammonia (gas) may be stored in the same room with chlorine (gas).",
+    "options": [],
+    "answer": "FALSE"
+  },
+  {
+    "id": "36",
+    "type": "true_false",
+    "question": "\"When working with chlorine",
+    "options": [],
+    "answer": "which are most often found in the control valve.\""
+  },
+  {
+    "id": "37",
+    "type": "true_false",
+    "question": "Ferric chloride is a very corrosive compound and should be treated as you would with any acid.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "38",
+    "type": "true_false",
+    "question": "The most common form of iron in solution in groundwater and in water under anaerobic conditions (bottom of a lake or reservoir) is ferric ion.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "39",
+    "type": "true_false",
+    "question": "Nitrate represents the least completely oxidized form of nitrogen found in water.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "40",
+    "type": "true_false",
+    "question": "Chloroform is the most common THM found in drinking water.",
+    "options": [],
+    "answer": "FALSE"
+  },
+  {
+    "id": "41",
+    "type": "true_false",
+    "question": "EPA regulations change frequently as new information becomes available.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "42",
+    "type": "true_false",
+    "question": "MCLs apply whether the contaminant is from naturally occurring sources or from man-made pollution.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "43",
+    "type": "true_false",
+    "question": "The health risk from mercury is less from mercury in fish than simply from waterborne mercury.",
+    "options": [],
+    "answer": 0
+  },
+  {
+    "id": "44",
+    "type": "true_false",
+    "question": "The presence of coliform bacteria indicates that other harmful organisms may be present in the water.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "45",
+    "type": "true_false",
+    "question": "Tier 1 violations are any situations in which even a brief violation or short term exposure to a contaminant has the potential to cause serious negative effect on human health.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "46",
+    "type": "true_false",
+    "question": "\"No discipline problem ever solves itself",
+    "options": [],
+    "answer": "the better will be the outcome.\""
+  },
+  {
+    "id": "47",
+    "type": "true_false",
+    "question": "Written communication is less demanding than oral communication and requires less careful preparation.",
+    "options": [],
+    "answer": "FALSE"
+  },
+  {
+    "id": "48",
+    "type": "true_false",
+    "question": "Customer calls are frequently your first indication that something may be wrong.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "49",
+    "type": "true_false",
+    "question": "\"Corrective maintenance",
+    "options": [],
+    "answer": "is considered reactive maintenance.\""
+  },
+  {
+    "id": "50",
+    "type": "true_false",
+    "question": "The SCADA system allows operators to enhance the efficiency of their water facility by keeping them fully informed and fully in control.",
+    "options": [],
+    "answer": "TRUE"
+  },
+  {
+    "id": "51",
+    "type": "true_false",
+    "question": "\"The operator should always call to the supervisor's attention unsafe conditions",
+    "options": [],
+    "answer": "and equipment or other concerns about the work they are performing.\""
+  },
+  {
+    "id": "52",
+    "type": "true_false",
+    "question": "On-the-Job-Training (OJT) is a good way of preventing accidents with an inexperienced operator.",
+    "options": [],
+    "answer": "FALSE"
+  }
+]
